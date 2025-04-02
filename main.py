734a4eba387eaa5b93857505ed5151942e126535
= <=>
2025-04-02 15:01:15 -0300

    send mail to user (using practice site)

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+import smtplib
+
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 import requests
+
+load_dotenv()
 
 practice_url = "https://appbrewery.github.io/instant_pot/"
 live_url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
@@ -17,3 +23,24 @@ price_without_currency = price.split("$")[1]
 
 # Convert to floating point number
 price_as_float = float(price_without_currency)
+
+# ------- Send mail -------
+title = soup.find(id="productTitle").get_text().strip()
+
+BUY_PRICE = 100
+EMAIL = os.getenv("EMAIL")
+PASS = os.getenv("PASS")
+SMTP_ADDR = os.getenv("SMTP_ADDR")
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is on a sale for {price}!"
+
+    with smtplib.SMTP(SMTP_ADDR, port=587) as connection:
+        connection.ehlo()
+        connection.starttls()
+        result = connection.login(EMAIL, PASS)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{practice_url}".encode("utf-8")
+        )
